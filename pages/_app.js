58db8d32c81ec0e81247f67fad56783b5d34b9a9
Tle Nattapong <nattapong@pluseducenter.com>
2025-03-25 15:37:22 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+  <Head>
+    <title>POPS</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    {/* tiktok verification */}
+    <meta name="tiktok-verification" content="2hpLNIo6gUwD61ECqfHNJrRLOvHUyl9M" />
+  </Head>
+  
+  <Component {...pageProps} /></>;
 }
