ce40504678e6905ef123a44f1ba23a7e601b982a
Tle Nattapong <nattapong@pluseducenter.com>
2025-03-28 15:44:33 +0700

    Update [...slug].jsx

--- pages/auth/social/detail/[...slug].jsx
+++ pages/auth/social/detail/[...slug].jsx
@@ -12,6 +12,7 @@ import {
     BarChart,
     PieChart
 } from "@mui/x-charts";
+import Image from "next/image";
 import { useRouter } from "next/router";
 export default function SocialDetail() {
   const { user } = useUserContext();
