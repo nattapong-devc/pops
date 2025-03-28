4fc21a6d7fb1e344eaf098fb9a1e3ecb610bda09
Tle Nattapong <nattapong@pluseducenter.com>
2025-03-28 17:12:56 +0700

    Update [...slug].jsx

--- pages/auth/success/[...slug].jsx
+++ pages/auth/success/[...slug].jsx
@@ -2,14 +2,16 @@ import { Box, Container, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
 import { useRouter } from "next/router";
-
+import ErrorOutlineRoundedIcon from "@mui/icons-material/ErrorOutlineRounded";
 const Success = () => {
   const router = useRouter();
   const { code } = router.query;
+  const [codeKey, setCodeKey] = useState(null);
 
   useEffect(() => {
     if (code && router.query.slug) {
       localStorage.setItem(`oauth_code_${router.query.slug}`, code);
+      setCodeKey(code);
 
       setTimeout(() => {
         window.close();
@@ -24,33 +26,68 @@ const Success = () => {
         height: "100dvh",
       }}
     >
-      <Box
-        sx={{
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          p: 3,
-          gap: 5,
-        }}
-      >
-        <CheckCircleRoundedIcon
-          sx={{
-            fontSize: 150,
-            color: "green",
-          }}
-        />
-        <Typography
-          sx={{
-            fontSize: 24,
-          }}
-          fontWeight={"bold"}
-          textAlign={"center"}
-        >
-          Successfully connected
-        </Typography>
-      </Box>
+      {codeKey ? (
+        <>
+          {" "}
+          <Box
+            sx={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              p: 3,
+              gap: 5,
+            }}
+          >
+            <CheckCircleRoundedIcon
+              sx={{
+                fontSize: 150,
+                color: "green",
+              }}
+            />
+            <Typography
+              sx={{
+                fontSize: 24,
+              }}
+              fontWeight={"bold"}
+              textAlign={"center"}
+            >
+              Successfully connected
+            </Typography>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box
+            sx={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              p: 3,
+              gap: 5,
+            }}
+          >
+            <ErrorOutlineRoundedIcon
+              sx={{
+                fontSize: 150,
+                color: "red",
+              }}
+            />
+            <Typography
+              sx={{
+                fontSize: 24,
+              }}
+              fontWeight={"bold"}
+              textAlign={"center"}
+            >
+              Successfully connected
+            </Typography>
+          </Box>
+        </>
+      )}
     </Container>
   );
 };
