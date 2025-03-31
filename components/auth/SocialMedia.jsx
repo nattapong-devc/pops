06ea8979ad76630332776b942a600846da6e1b57
Tle Nattapong <nattapong@pluseducenter.com>
2025-03-31 12:39:59 +0700

    Update SocialMedia.jsx

--- components/auth/SocialMedia.jsx
+++ components/auth/SocialMedia.jsx
@@ -55,14 +55,14 @@ export default function SocialMedia() {
       name: "Facebook",
       icon: <FacebookIcon />,
       loginUrl: facebookLoginUrl,
-      status: socialData.facebook,
+      status: socialData.facebook ? socialData.facebook : null,
     },
     {
       id: 2,
       name: "Instagram",
       icon: <InstagramIcon />,
       loginUrl: instagramLoginUrl,
-      status: socialData.instagram,
+      status: socialData.instagram ? socialData.instagram : null,
     },
   ]);
 
