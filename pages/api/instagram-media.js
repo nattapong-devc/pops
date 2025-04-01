20662b4e95eef472520fea8ba703f0a1101fa286
Tle Nattapong <nattapong@pluseducenter.com>
2025-04-01 13:50:27 +0700

    Update instagram-media.js

--- pages/api/instagram-media.js
+++ pages/api/instagram-media.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-
-    const { access_token , image_url , caption } = req.body;
+    const { access_token, image_url, caption } = req.body;
 
     try {
       const response = await axios.post(
@@ -17,20 +16,33 @@ export default async function handler(req, res) {
 
       console.log(response.data);
 
+      const { id } = response.data;
+
+      const publishResponse = await axios.post(
+        `https://graph.instagram.com/${id}/publish`,
+        {
+          access_token: access_token,
+        }
+      );
+
       res.status(200).json({
         status: "success",
-        data: response.data,
+        data: {
+          id: id,
+          publishResponse: publishResponse.data,
+          a,
+        },
       });
     } catch (error) {
-      console.error("Error:", error.response ? error.response.data : error.message);
+      console.error(
+        "Error:",
+        error.response ? error.response.data : error.message
+      );
       res.status(500).json({
         status: "error",
         message: error.response ? error.response.data : error.message,
       });
     }
-  
-  
-    
   } else {
     res.status(405).json({ error: "Method Not Allowed" });
   }
